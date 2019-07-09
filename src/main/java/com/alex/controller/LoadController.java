0fe11850279f1558dc591bd7df1ce6da1022674b
Alex Chaban <alex-chaban@mail.ru>
2019-07-09 23:14:19 +0300

    init

---
+++ src/main/java/com/alex/controller/LoadController.java
@@ -0,0 +1,20 @@
+package com.alex.controller;
+
+import com.alex.model.Word;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("test")
+public class LoadController {
+
+    @GetMapping
+    public List<Word> response() {
+        return Arrays.asList(new Word("Hello", "Привет"), new Word("Bye", "Пока"));
+    }
+
+}
---
+++ src/main/java/com/alex/model/Word.java
@@ -0,0 +1,13 @@
+package com.alex.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Word {
+    private String engName;
+    private String ruName;
+}
